8da533882c17454c90371082ad40f74599f85807
rafael <rafael.contatotrab@gmail.com>
2019-06-15 20:15:42 -0300

    finalizando projeto

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,18 +2,41 @@ function trazerDados() {
     const dadoComida = '../assets/comida.json'
     axios(dadoComida).then(resp => {
         const itens = resp.data.reduce(
-            (html, comida) => html + `<li class="item card"><img src="./assets${comida.src}"></li>`, ''
+            (html, comida) => html + `<li class="item card"><img src="./assets${comida.src}"><span class="nome" style="visibility: hidden;">${comida.Nome}</span></li>`, ''
         )
         $('#comidas').html(itens)
     })
-    const dadoPrato = '../assets/prato.json'
-    axios(dadoPrato).then(resp => {
-        const itens = resp.data.reduce(
-            (html, prato) => html + `<li class="item card"><img src="./assets${prato.src}"></li>`, ''
+    axios(dadoComida).then(resp => {
+        const pontos = resp.data.reduce(
+            (html, ponto) => html + `
+            <div class="divPonto"><span class="ponto" style="display: none;">${ponto.Pontos}</span> <span class="nomePonto" style="visibility: hidden;">${ponto.Nome}</span></div>`, ''
         )
-        $('#prato').html(itens)
+        $('#informacoes').html(pontos)
     })
 }
+
+function atualizarResultado() {
+    let nomes = $('#prato').find('span')
+    let nomesResultado = $('#resultado').find('.nomePonto')
+
+    let divPonto = document.querySelectorAll('.divPonto')
+    let soma;
+    if (soma == undefined)
+        soma = 0;
+
+    for (let i = 0; i < nomes.length; i++) {
+        for (let k = 0; k < nomesResultado.length; k++) {
+            if (nomes[i].innerHTML.trim() === nomesResultado[k].innerHTML.trim()) {
+                let pontos = parseInt(divPonto[k].firstChild.innerHTML)
+                soma = soma + pontos
+                $('#pontuacao').html(`<p class="texto">${soma}</p>`)
+            }
+        }
+    }
+
+    setTimeout(atualizarResultado, 1000)
+}
+
 function moverObjetos() {
     $(function () {
         $('#comidas, #prato').sortable({
@@ -27,7 +50,36 @@ function moverObjetos() {
     }).disableSelection()
 }
 
-trazerDados()
-setInterval(() => {
- moverObjetos()
-}, 500)
+function verificarSeExisteFilho() {
+    let itens = $('.prato li').length
+    if (itens == 0)
+        $('#pontuacao').html(`<p class="texto">${0}</p>`)
+
+    setTimeout(verificarSeExisteFilho, 2000)
+}
+
+function contatorDePontos() {
+    const pontuacaoAtual = document.getElementById('pontuacao')
+    let a = pontuacaoAtual.childNodes[0].innerText
+    let pontAtual = parseInt(a)
+    if (pontAtual <= 0)
+        $('#estado').html(`<p class="texto">Ruím</p>`)
+    else if (pontAtual > 0 && pontAtual < 2)
+        $('#estado').html(`<p class="texto">Bom</p>`)
+    else 
+    $('#estado').html(`<p class="texto">Ótimo</p>`)
+
+    setTimeout(contatorDePontos, 2000)
+}
+
+function load() {
+    trazerDados()
+    atualizarResultado()
+    verificarSeExisteFilho()
+    setInterval(() => {
+        moverObjetos()
+        contatorDePontos()
+    }, 500)
+}
+
+load()
